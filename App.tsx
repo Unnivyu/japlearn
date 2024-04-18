502888a131baa58dfe9f3b4bc5ec14574c9b0012
unknown <kerryambos884512345@gmail.com>
2024-04-18 23:11:22 +0800

    login and startmenu

--- App.tsx
+++ App.tsx
@@ -41,7 +41,7 @@ export default function App() {
   return (
     <NavigationContainer>
       
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Signup">
         <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }} />
