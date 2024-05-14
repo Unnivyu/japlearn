241aa26fe2e0cec8a89e8b0b406279d3cf7e1eff
Kerry Ambos <89137808+Keewwyy@users.noreply.github.com>
2024-05-14 19:43:43 +0800

    Delete src directory

--- src/main/java/japlearn/demo/Repository/QuackslateIntroRepository.java
+++
@@ -1,9 +0,0 @@
-package japlearn.demo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import japlearn.demo.Entity.QuackslateIntro;
-
-public interface QuackslateIntroRepository extends MongoRepository<QuackslateIntro, Long> {
-    QuackslateIntro findByJapPhrase(String japPhrase);
-}
